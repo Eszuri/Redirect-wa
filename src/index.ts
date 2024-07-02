deb5183f7f97dfdb14d8e44f761dc81fe7199ff1
Eszuri <ualdo296@gmail.com>
2024-07-02 16:51:57 +0700

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { fileURLToPath } from "url";
+import path from "path";
+// import { fileURLToPath } from "url";
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -17,4 +18,4 @@ app.listen(3000, () => {
     console.log("express Running");
 })
 
-export default app;
+// export default app;
