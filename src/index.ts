96b1b333c3301e7a1713147a50da58ec1437941a
Eszuri <ualdo296@gmail.com>
2024-07-02 17:40:25 +0700

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
 import express from "express";
 import path from "path";
-// import { fileURLToPath } from "url";
 const app = express();
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
+// tampilan home
 app.get("/", (req, res) => {
     res.render('index', { title: "This Is Template Engine" })
-})
+});
+
+
+
 
 app.listen(3000, () => {
     console.log("express Running");
 })
-
-// export default app;
