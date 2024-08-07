96a28574925d85eddd9159da4e5cc39940045f52
ElynaRia <elynaria28@gmail.com>
2024-08-07 17:05:19 +0700

    first coomited

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,26 @@
-import "module-alias/register"
+import "module-alias/register";
 import express from "express";
 import path from "path";
-import data from "@root/controller/data";
+import data from "./controller/data";
 const app = express();
 
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // tampilan home
 app.get("/", (req, res) => {
-    res.render('index', { title: "This Is Template Engine" })
+  res.render("index", { title: "This Is Template Engine" });
 });
 
 // endpoint test
-app.get("/test", data)
-
+app.get("/test", data);
 
 // "_moduleAliases": {
 //     "@data": "build"
 // }
 
-
 app.listen(3000, () => {
-    console.log("express Running");
-})
+  console.log("express Running");
+});
+
